5537d03911985f3f896999dc6c05568926b57040
Amogh Maheshchandra Bhabal Nolastname <abhabal@sundevils.asu.edu>
2019-07-18 16:20:15 -0700

    Add project files.

---
+++ WebBrowser/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace WebBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Browser = new System.Windows.Forms.WebBrowser();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.EncryptRes = new System.Windows.Forms.Label();
+            this.Encrypt = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.DecryptRes = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Browser
+            // 
+            this.Browser.Location = new System.Drawing.Point(0, 28);
+            this.Browser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.Browser.Name = "Browser";
+            this.Browser.Size = new System.Drawing.Size(700, 333);
+            this.Browser.TabIndex = 0;
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(0, 2);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(661, 20);
+            this.txtUrl.TabIndex = 1;
+            this.txtUrl.Text = "http://";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(668, 2);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(32, 23);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.BtnGo_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(458, 458);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(46, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(510, 458);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(57, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Subtract";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(573, 458);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(57, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Multiply";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(633, 458);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(57, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Divide";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(458, 423);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(590, 423);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(468, 404);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "1st Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(590, 404);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "2nd Number";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(564, 488);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "=";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(555, 510);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Result";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(528, 377);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 18);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Calculator";
+            // 
+            // EncryptRes
+            // 
+            this.EncryptRes.AutoSize = true;
+            this.EncryptRes.Location = new System.Drawing.Point(145, 458);
+            this.EncryptRes.Name = "EncryptRes";
+            this.EncryptRes.Size = new System.Drawing.Size(37, 13);
+            this.EncryptRes.TabIndex = 14;
+            this.EncryptRes.Text = "Result";
+            this.EncryptRes.Click += new System.EventHandler(this.EncryptRes_Click);
+            // 
+            // Encrypt
+            // 
+            this.Encrypt.Location = new System.Drawing.Point(304, 436);
+            this.Encrypt.Name = "Encrypt";
+            this.Encrypt.Size = new System.Drawing.Size(75, 23);
+            this.Encrypt.TabIndex = 15;
+            this.Encrypt.Text = "Encrypt";
+            this.Encrypt.UseVisualStyleBackColor = true;
+            this.Encrypt.Click += new System.EventHandler(this.Encrypt_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(185, 410);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(227, 20);
+            this.textBox3.TabIndex = 16;
+            // 
+            // DecryptRes
+            // 
+            this.DecryptRes.AutoSize = true;
+            this.DecryptRes.Location = new System.Drawing.Point(145, 471);
+            this.DecryptRes.Name = "DecryptRes";
+            this.DecryptRes.Size = new System.Drawing.Size(37, 13);
+            this.DecryptRes.TabIndex = 17;
+            this.DecryptRes.Text = "Result";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(24, 413);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(155, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Enter the Text to be Encrypted:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(24, 441);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(221, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Click the \"Encrypt\" button to encrypt the text:";
+            this.label7.Click += new System.EventHandler(this.Label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(24, 458);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(107, 13);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Your Encrypted Text:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(24, 471);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(108, 13);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "The Decrypted Text: ";
+            this.label9.Click += new System.EventHandler(this.Label9_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(145, 377);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(145, 18);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Encryptor/ Decryptor";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 601);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.DecryptRes);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.Encrypt);
+            this.Controls.Add(this.EncryptRes);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.Browser);
+            this.Name = "Form1";
+            this.Text = "Amogh\'s Browser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser Browser;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label EncryptRes;
+        private System.Windows.Forms.Button Encrypt;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label DecryptRes;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ WebBrowser/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// This form is for the Browser, for Assignment 1 - task 5, and Assignment 2
+namespace WebBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGo_Click(object sender, EventArgs e)    // Clicking this button will have the browser navigate to the webpage
+        {                                                       // whose address is specified in textbox "txtUrl"
+            Browser.Navigate(txtUrl.Text);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)  // This button will perform addition in calculator
+        {
+            float a = float.Parse(textBox1.Text);               // "a" variable will get the value in left textbox "textBox1"
+            float b = float.Parse(textBox2.Text);               // "b" variable will get the value in right textbox "textBox2"
+            a = a + b;                                          // addition
+            label4.Text = a.ToString();                         // label "label4" will get the value of "a" variable
+        }
+
+        private void Button2_Click(object sender, EventArgs e)  // This button will perform subtraction in calculator
+        {
+            float a = float.Parse(textBox1.Text);           // "a" variable will get the value in left textbox "textBox1"
+            float b = float.Parse(textBox2.Text);           // "b" variable will get the value in right textbox "textBox2"
+            a = a - b;                                      // subtraction
+            label4.Text = a.ToString();                     // label "label4" will get the value of "a" variable
+        }
+
+        private void Button3_Click(object sender, EventArgs e)      // This button will perform multiplication in calculator
+        {
+            float a = float.Parse(textBox1.Text);           // "a" variable will get the value in left textbox "textBox1"
+            float b = float.Parse(textBox2.Text);           // "b" variable will get the value in right textbox "textBox2"
+            a = a * b;                                      // multiplication
+            label4.Text = a.ToString();                     // label "label4" will get the value of "a" variable
+        }
+
+        private void Button4_Click(object sender, EventArgs e)       // This button will perform division in calculator
+        {
+            float a = float.Parse(textBox1.Text);           // dividend "a" variable will get the value in left textbox "textBox1"
+            float b = float.Parse(textBox2.Text);           // divisor "b" variable will get the value in right textbox "textBox2"
+            a = a / b;                                      // division
+            label4.Text = a.ToString();                     // label "label4" will get the value of "a" variable
+        }
+
+        private void Encrypt_Click(object sender, EventArgs e)      // The encryptor and decryptor form (uses service at ASU reporsitory: 
+        {                                                           // http://neptune.fulton.ad.asu.edu/WSRepository/Services/EncryptionWcf/Service.svc)
+            EncryptServicer.ServiceClient Encrypter = new EncryptServicer.ServiceClient();  // initializing a client object of EncryptServcier
+            try{ EncryptRes.Text = Encrypter.Encrypt(textBox3.Text); }              // try catch statement in which "EncryptRes" label is given the encrypted value of text in "textBox3", using Encrypt() function
+            catch (Exception ec) { EncryptRes.Text = ec.Message.ToString(); }       // catch statement in which error message is stored into label "EncryptRes"
+            try { DecryptRes.Text = Encrypter.Decrypt(EncryptRes.Text); }           // try catch statement in which "DecryptRes" label is given the decrypted value of text in "EncryptRes", using Decrypt() function
+            catch (Exception ec) { DecryptRes.Text = ec.Message.ToString(); }       // catch statement in which error message is stored into label "DecryptRes"
+        }
+
+        private void Label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EncryptRes_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void Label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
